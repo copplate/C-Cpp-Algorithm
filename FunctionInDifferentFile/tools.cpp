baaaba1d1c87f93a39cf668dffae5b2e9e1bfb67
yuancheng <674678976@qq.com>
2023-10-28 17:22:25 +0800

    吴哥函数分文件编写

---
+++ FunctionInDifferentFile/tools.cpp
@@ -0,0 +1,12 @@
+//
+// Created by wyc on 2023/9/13.
+//
+#include "tools.h"
+int max(int a,int b){
+    cout << "ok" << endl;
+    return a > b ? a : b;
+}
+
+int min(int a,int b){
+    return a < b ? a : b;
+}
---
+++ FunctionInDifferentFile/tools.h
@@ -0,0 +1,13 @@
+//
+// Created by wyc on 2023/9/13.
+//
+
+#ifndef CPPPRAC1_TOOLS_H
+#define CPPPRAC1_TOOLS_H
+
+#endif //CPPPRAC1_TOOLS_H
+#include <iostream>//包含头文件
+using namespace std;//指定缺省的命名空间
+
+int max(int a, int b);
+int min(int a, int b);
